2cf8e36b4937e26813655a581389d8438996315c
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-08-01 19:51:09 -0500

    Updated components

---
+++ angular_practice/angular-studio-part1/src/app/experiments/experiments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
